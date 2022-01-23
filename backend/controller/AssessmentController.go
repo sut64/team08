f63365a13a1db4cd2197899e93d98637ca15d1ca
BearishStaff <bearishtstaff@gmail.com>
2022-01-23 14:08:04 +0700

    update - Assessment controller

---
+++ backend/controller/AssessmentController.go
@@ -0,0 +1,66 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sut64/team08/entity"
+)
+
+func CreateAssessment(c *gin.Context) {
+	var assessment entity.Assessment
+	if err := c.ShouldBindJSON(&assessment); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	if err := entity.DB().Create(&assessment).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"data": assessment})
+}
+
+func GetAssessment(c *gin.Context) {
+	var assessment entity.Assessment
+	id := c.Param("id")
+	if err := entity.DB().Raw("SELECT * FROM assessments WHERE id = ?", id).Scan(&assessment).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"data": assessment})
+}
+
+func ListAssessment(c *gin.Context) {
+	var assessment []entity.Assessment
+	if err := entity.DB().Raw("SELECT * FROM assessments").Scan(&assessment).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"data": assessment})
+}
+
+func DeleteAssessment(c *gin.Context) {
+	id := c.Param("id")
+	if tx := entity.DB().Exec("DELETE FROM assessments WHERE id = ?", id); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "assessment not found"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"data": id})
+}
+
+func Updateassessment(c *gin.Context) {
+	var assessment entity.Assessment
+	if err := c.ShouldBindJSON(&assessment); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	if tx := entity.DB().Where("id = ?", assessment.ID).First(&assessment); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "assessment not found"})
+		return
+	}
+	if err := entity.DB().Save(&assessment).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"data": assessment})
+}
