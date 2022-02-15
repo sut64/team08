61ddc25185d8c6a28052edf4ad014e7276101453
unknown <b6213954@g.sut.ac.th>
2022-02-15 17:53:11 +0700

    Update Data Base

--- backend/entity/schema.go
+++ backend/entity/schema.go
@@ -20,9 +20,9 @@ type Patient struct {
 type AmbulanceOnDuty struct {
 	gorm.Model
 
-	Code       string    `valid:"matches(^[D]\\d{8}$)~Code not matches 'Dxxxxxxxx' x = number,required~Code must not be blank"`
+	Code       string    `gorm:"uniqueIndex" valid:"matches(^[D]\\d{8}$)~Code not matches 'Dxxxxxxxx' ex. D00000000,required~Code must not be blank"`
 	OnDutyDate time.Time `valid:"today~Date must be today"`
-	Passenger  uint      `valid:"required~Passenger must be greater than zero"`
+	Passenger  int       `valid:"required~Passenger must not be zero, positive~Passenger must be greater than zero"`
 
 	AmbulanceID *uint
 	Ambulance   Ambulance `gorm:"references:id" valid:"-"`
@@ -38,7 +38,7 @@ type AmbulanceOnDuty struct {
 
 type AmbulanceArrival struct {
 	gorm.Model
-	Number_of_passenger int       `valid:"required,Positivenumber~must be greater than equal to zero"`
+	Number_of_passenger int       `valid:"required,positive~must be greater than equal to zero"`
 	Distance            float32   `valid:"required,Positivedecimal~must be greater to zero"`
 	DateTime            time.Time `valid:"today~Ambulance Arrival must be current date"`
 
@@ -71,7 +71,7 @@ type Assessment struct {
 
 type Ambulance struct {
 	gorm.Model
-	CarNumber    int       `valid:"required,mtzero"`
+	CarNumber    int       `gorm:"uniqueIndex" valid:"required,positive"`
 	Registration string    `valid:"matches(^[ก-ฮ]{2}\\d{1}$|^[ก-ฮ]{2}\\d{2}$|^[ก-ฮ]{2}\\d{3}$|^[ก-ฮ]{2}\\d{4}$|^1[ก-ฮ]{2}\\d{1}$|^1[ก-ฮ]{2}\\d{2}$|^1[ก-ฮ]{2}\\d{3}$|^1[ก-ฮ]{2}\\d{4}$)"`
 	DateTime     time.Time `valid:"today~AmbulanceDate not true"`
 
@@ -184,26 +184,9 @@ func init() {
 		n := i.(int)
 		return n >= 1
 	})
-
-	govalidator.CustomTypeTagMap.Set("mtzero", func(i interface{}, context interface{}) bool {
-		c, _ := i.(int)
-		return c > 0
-	})
-	govalidator.CustomTypeTagMap.Set("Positivenumber", func(i interface{}, context interface{}) bool {
-		n := i.(int)
-		if n <= 0 {
-			return false
-		} else {
-			return true
-		}
-	})
 	govalidator.CustomTypeTagMap.Set("Positivedecimal", func(i interface{}, context interface{}) bool {
 		d := i.(float32)
-		if d <= 0.0 {
-			return false
-		} else {
-			return true
-		}
+		return d > 0.00
 	})
 
 }
